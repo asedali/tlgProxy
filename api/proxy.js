5a1fb75dc8cd9a05875326e4f7453db5799f2f49
aSedali <toseyyedali@gmail.com>
2025-02-24 15:36:15 +0330

    Update and rename proxy.js to api/proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,10 +1,11 @@
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
   try {
     const response = await fetch('https://api.telegram.org' + req.url);
     const data = await response.json();
-    
+
+    res.setHeader("Access-Control-Allow-Origin", "*"); // Fix CORS issues
     res.status(200).json(data);
   } catch (error) {
     res.status(500).json({ error: "Error connecting to Telegram API", details: error.message });
   }
-};
+}
